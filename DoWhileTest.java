a5daa30d4bddaaa9de39a18c67f1b812e1920da6
ShinKyoungJu-2016039061 <32188305+ShinKyoungJu-2016039061@users.noreply.github.com>
2017-09-22 15:10:05 +0900

    first upload in Git

---
+++ DoWhileTest.java
@@ -0,0 +1,15 @@
+
+public class DoWhileTest 
+{
+	public static void main(String args[])
+	{
+		int a=0;
+		do
+		{
+			
+			System.out.println("a=" + a);
+			a++;
+		}
+		while(a < 10);
+	}
+}
---
+++ TestIfElse.java
@@ -0,0 +1,33 @@
+
+public class TestIfElse {
+	
+	public static void main(String args[] )
+
+	{
+		
+		int jumsu = 40;
+		
+		if(jumsu >= 60)
+			
+		{
+			
+			System.out.println("당신은 합격 입니다");
+			
+				
+		}
+	
+		else
+		{
+			System.out.println("당신은 불합격 입니다");
+			
+		}
+	
+	
+		System.out.println("당신의 점수는 " + jumsu + "입니다");
+	
+	}
+	
+	
+	
+	
+}
