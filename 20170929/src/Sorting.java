03b6eecb2f36bac8d84cab2873fdd9d0e219307e
shinkj <bumblebee151112@daum.net>
2017-09-29 16:42:34 +0900

    edeed

---
+++ 20170929/src/Sorting.java
@@ -0,0 +1,35 @@
+
+public class Sorting {
+	
+	public static void main(String[] args)
+	{
+		int test []  = {20,30,40,50,10};
+		int i,j,temp;
+		
+		System.out.println("정렬전");
+		for( i =0; i<5;i++)
+		{
+			
+			System.out.println((i+1)+ "번째 데이터" + test [i]);
+			
+		}
+		
+		for( i =0 ;i<4 ;i++)
+		{
+			
+			for(j=i+1 ; j<5 ; j++)
+			{
+				
+				if(test[i] < test[j])
+				{
+					
+					
+					temp = test[i];
+					test[i]=test[j];
+					test[j]= temp;
+				}
+			}
+		}
+	}
+
+}
