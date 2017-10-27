cc8b7da2eca3c57ca9881cee8cc2eeeb1ee87fc6
shinkj <bumblebee151112@daum.net>
2017-10-27 16:17:21 +0900

    1027 sdfsdfd

---
+++ 1027/src/ExceptionExist.java
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+class ExceptionExist {
+
+	public static void main(String[] args) 
+	{
+		func();
+	}
+
+	static void func() {
+
+		int i = 1;
+		int j = 0;
+		System.out.println(j / i);
+	}
+
+}
